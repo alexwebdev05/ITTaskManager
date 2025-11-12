6966e5b096ffd658d14dff1e32fe8568f21166c5
alexwebdev05 <alexwebdev05@proton.me>
2025-11-12 12:44:39 +0100

    create new task and list tasks functional

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod task_manager;
 
-use std::io;
+use std::io::{self, Write};
 
 use task_manager::{TaskManager, TaskPriority, TaskStatus};
 
@@ -8,51 +8,64 @@ fn main() {
     // Set up task manager
     let mut manager = TaskManager::new();
 
-    // Cli
-    println!("##### TaskManager #####\n");
-    println!("Choose one of those options:");
-    println!(" 1: Create a new task");
-    println!(" 2: List tasks");
-    println!(" 3: Change task state");
-    println!(" 4: Filter by priority");
-    println!(" 5: Close");
+    // Loop
+    loop {
+        // Cli
+        println!("\n##### TaskManager #####\n");
+        println!("Choose one of those options:");
+        println!(" 1: Create a new task");
+        println!(" 2: List tasks");
+        println!(" 3: Change task state");
+        println!(" 4: Filter by priority");
+        println!(" 5: Close\n");
 
-    // Get response
-    let mut input = String::new();
-    io::stdin().read_line(&mut input);
+        // Get response
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-    match input.trim() {
-        "1" => {
-            let mut title = String::new();
-            let mut description = String::new();
-            let mut priority: TaskPriority;
-            let status = TaskStatus::Pending;
+        match input.trim() {
+            "1" => {
+                let mut title = String::new();
+                let mut description = String::new();
+                let priority: TaskPriority;
+                let status = TaskStatus::Pending;
 
-            println!("Enter a title: ");
-            io::stdin().read_line(&mut title);
-            
-            println!("Enter a description: ");
-            io::stdin().read_line(&mut description);
-            
-            let mut priority_option = String::new();
-            println!("Select the priority: ");
-            println!(" 1: Critical");
-            println!(" 2: High");
-            println!(" 3: Medium");
-            println!(" 4: Low");
-            io::stdin().read_line(&mut priority_option);
+                print!("Enter a title: ");
+                io::stdout().flush().unwrap();
+                io::stdin().read_line(&mut title).unwrap();
 
-            match priority_option.trim() {
-                "1" => priority = TaskPriority::Critical,
-                "2" => priority = TaskPriority::High,
-                "3" => priority = TaskPriority::Medium,
-                "4" => priority = TaskPriority::Low,
-                _ => priority = TaskPriority::Medium,
+                print!("Enter a description: ");
+                io::stdout().flush().unwrap();
+                io::stdin().read_line(&mut description).unwrap();
+
+                let mut priority_option = String::new();
+                println!("\n\nSelect the priority: ");
+                println!(" 1: Critical");
+                println!(" 2: High");
+                println!(" 3: Medium");
+                println!(" 4: Low\n");
+                io::stdin().read_line(&mut priority_option).unwrap();
+
+                match priority_option.trim() {
+                    "1" => priority = TaskPriority::Critical,
+                    "2" => priority = TaskPriority::High,
+                    "3" => priority = TaskPriority::Medium,
+                    "4" => priority = TaskPriority::Low,
+                    _ => priority = TaskPriority::Medium,
+                }
+
+                manager.add_task(
+                    title.trim().to_string(),
+                    description.trim().to_string(),
+                    priority,
+                    status,
+                );
+                println!("Task added correctly.")
             }
-
-            manager.add_task(title.trim().to_string(), description.trim().to_string(), priority, status);
-            manager.list_tasks();
-        },
-        _ => print!("Select an option")
+            "2" => {
+                manager.list_tasks();
+            }
+            _ => print!("Select an option"),
+        }
     }
 }
